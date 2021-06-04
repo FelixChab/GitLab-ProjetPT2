bbecb948b0f00d08c678d264eb5a61c31f151947
Félix Chabellard <felixchabellard33@gmail.com>
2021-06-04 15:35:48 +0200

    US4 done

--- ProjetPT2K/ProjetPT2K/Admin.cs
+++ ProjetPT2K/ProjetPT2K/Admin.cs
@@ -12,5 +12,16 @@ namespace ProjetPT2K
         {
 
         }
+
+        public List<ALBUMS> GetLateLoans()
+        {
+            MusiquePT2_KEntities connection = Database.GetInstance().GetConnection();
+            List<ALBUMS> lateLoans = (from a in connection.ALBUMS
+                                  join e in connection.EMPRUNTER
+                                  on a.CODE_ALBUM equals e.CODE_ALBUM
+                                  where e.DATE_RETOUR_ATTENDU < e.DATE_RETOUR select a).ToList();
+            return lateLoans;
+        }
+
     }
 }
