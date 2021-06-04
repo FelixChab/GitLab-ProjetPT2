a43c0d8b3015c04e63296ca838a4c88a0409fb21
manboisson <manon.boisson.1@iut.u-bordeaux.fr>
2021-06-04 14:32:35 +0200

    documentation of database.

--- ProjetPT2K/ProjetPT2K/Database.cs
+++ ProjetPT2K/ProjetPT2K/Database.cs
@@ -7,12 +7,15 @@ namespace ProjetPT2K
 	{
 		private static Database _Database;
 
-		private readonly MusiquePT2_KEntities Connection;
+		private readonly MusiquePT2_KEntities Connection; // allows the connection
 		
 		private Database() {
 			this.Connection = new MusiquePT2_KEntities();
 		}
 
+		/**
+		 * Function that allows the instanciation of the database.
+		 */
 		public static Database GetInstance()
 		{
 			if (_Database == null)
@@ -22,11 +25,19 @@ namespace ProjetPT2K
 			return _Database;
 		}
 
+		/**
+		 * Function responsible of the connection with the database.
+		 */
 		public MusiquePT2_KEntities GetConnection()
         {
 			return this.Connection;
         }
 
+		/**
+		 * Function that allows a subscriber to log itself the application.
+		 * @param login, the login of the subscriber.
+		 * @param password, the passeword of the subscriber.
+		 * */
 		public ABONNÉS Login(string login, string password)
 		{
 			var request = from subscriber in this.Connection.ABONNÉS
