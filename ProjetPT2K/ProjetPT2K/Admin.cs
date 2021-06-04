d48e0a273cb193b53108bca27d53aa81231b6f74
Juanito <juanitoo.fr@gmail.com>
2021-06-04 15:33:40 +0200

    us5 done

--- ProjetPT2K/ProjetPT2K/Admin.cs
+++ ProjetPT2K/ProjetPT2K/Admin.cs
@@ -12,5 +12,16 @@ namespace ProjetPT2K
         {
 
         }
+
+        public List<ABONNÉS> getLateSubscribers()
+        {
+            MusiquePT2_KEntities connection = Database.GetInstance().GetConnection();
+            List<ABONNÉS> lateSubscribers = (from a in connection.ALBUMS
+                                  join e in connection.EMPRUNTER
+                                  on a.CODE_ALBUM equals e.CODE_ALBUM
+                                  where (e.DATE_EMPRUNT.AddDays(10).CompareTo(new DateTime()) <= 0)
+                                  from s in connection.ABONNÉS where s.CODE_ABONNÉ==e.CODE_ABONNÉ select s).ToList();
+            return lateSubscribers;
+        }
     }
 }
