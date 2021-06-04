d9b7de6be5413375ee9019b289a7893379be2032
Félix Chabellard <felixchabellard33@gmail.com>
2021-06-04 10:52:52 +0200

    ajout code sln

---
+++ ProjetPT2K/ProjetPT2K/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetPT2K
+{
+    public static class App
+    {
+        /// <summary>
+        /// Application main starting point
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+        }
+
+        // ...
+
+    }
+}
