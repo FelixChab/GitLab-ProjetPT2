c65f70ff3d5634373f8afa0713f0ec3ecdeb9e52
Moins Alexis <alexis.moins@etu.u-bordeaux.fr>
2021-06-07 09:18:30 +0200

    Adeed account creation form

---
+++ ProjetPT2K/ProjetPT2K/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetPT2K
+{
+    public partial class PAYS
+    {
+        /**
+         * Return the string representation of the counrty.
+         */
+        public override String ToString()
+        {
+            return NOM_PAYS;
+        }
+    }
+}
