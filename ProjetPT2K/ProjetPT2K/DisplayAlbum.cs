438bf7b45475e67701cc29c9bfa1485f05d64103
pbernini <paul.bernini@etu.u-bordeaux.fr>
2021-06-08 17:13:11 +0200

    Test

--- ProjetPT2K/ProjetPT2K/DisplayAlbum.cs
+++ ProjetPT2K/ProjetPT2K/DisplayAlbum.cs
@@ -10,7 +10,10 @@ namespace ProjetPT2K
 {
     class DisplayAlbum : ALBUMS
     {
+        //List<ALBUMS> albums;
         private ALBUMS album;
+        //private List<byte[]> pictures { get; set; }
+
         private byte[] picture { get; set; }
         private string title;
         public string Title { get { return this.title; } set { this.title = TITRE_ALBUM; } }
@@ -37,6 +40,9 @@ namespace ProjetPT2K
         /// </summary>
         public Image GetImage()
         {
+            List<Image> images = new List<Image>();
+            //foreach (byte[] pictureInBytes in pictures)
+            //{
             MemoryStream ms = new MemoryStream(picture);
             Bitmap img = new Bitmap (ms);
             //img.SetResolution(ImageWidth.Width, ImageWidth.Height);
--- ProjetPT2K/ProjetPT2K/MainView.designer.cs
+++ ProjetPT2K/ProjetPT2K/MainView.designer.cs
@@ -29,9 +29,14 @@ namespace ProjetPT2K
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainView));
             this.recommandationsLabel = new System.Windows.Forms.Label();
+            this.mainMenuListBox = new System.Windows.Forms.ListBox();
+            this.albumTitleTextBox = new System.Windows.Forms.TextBox();
             this.tagRecommandations = new System.Windows.Forms.Label();
             this.tagBestAlbums = new System.Windows.Forms.Label();
+            this.goToAcountView = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.goToAcountView)).BeginInit();
             this.mostBorrowedAlbumsLabel = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
@@ -45,6 +50,25 @@ namespace ProjetPT2K
             this.recommandationsLabel.Size = new System.Drawing.Size(185, 29);
             this.recommandationsLabel.TabIndex = 9;
             this.recommandationsLabel.Text = "Recommandations";
+            // 
+            // mainMenuListBox
+            // 
+            this.mainMenuListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(129)))), ((int)(((byte)(172)))));
+            this.mainMenuListBox.FormattingEnabled = true;
+            this.mainMenuListBox.Location = new System.Drawing.Point(12, 12);
+            this.mainMenuListBox.Name = "mainMenuListBox";
+            this.mainMenuListBox.Size = new System.Drawing.Size(179, 433);
+            this.mainMenuListBox.TabIndex = 10;
+            this.mainMenuListBox.SelectedIndexChanged += new System.EventHandler(this.mainMenuListBox_SelectedIndexChanged);
+            // 
+            // albumTitleTextBox
+            // 
+            this.albumTitleTextBox.Enabled = false;
+            this.albumTitleTextBox.Location = new System.Drawing.Point(231, 12);
+            this.albumTitleTextBox.Name = "albumTitleTextBox";
+            this.albumTitleTextBox.Size = new System.Drawing.Size(372, 20);
+            this.albumTitleTextBox.TabIndex = 11;
+            this.albumTitleTextBox.Visible = false;
             // 
             // tagRecommandations
             // 
@@ -83,6 +107,14 @@ namespace ProjetPT2K
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(3, 467);
             this.panel1.TabIndex = 17;
+            this.goToAcountView.Image = ((System.Drawing.Image)(resources.GetObject("goToAcountView.Image")));
+            this.goToAcountView.Location = new System.Drawing.Point(753, 12);
+            this.goToAcountView.Name = "goToAcountView";
+            this.goToAcountView.Size = new System.Drawing.Size(39, 35);
+            this.goToAcountView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.goToAcountView.TabIndex = 15;
+            this.goToAcountView.TabStop = false;
+            this.goToAcountView.Click += new System.EventHandler(this.GoToAccountView_Click);
             // 
             // MainView
             // 
@@ -93,14 +125,18 @@ namespace ProjetPT2K
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.mostBorrowedAlbumsLabel);
+            this.Controls.Add(this.goToAcountView);
             this.Controls.Add(this.tagBestAlbums);
             this.Controls.Add(this.tagRecommandations);
+            this.Controls.Add(this.albumTitleTextBox);
+            this.Controls.Add(this.mainMenuListBox);
             this.Controls.Add(this.recommandationsLabel);
             this.Name = "MainView";
             this.Text = "MainView";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainView_Paint);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainView_MouseClick);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainView_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.goToAcountView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,9 +145,12 @@ namespace ProjetPT2K
         #endregion
 
         private System.Windows.Forms.Label recommandationsLabel;
+        private System.Windows.Forms.ListBox mainMenuListBox;
+        private System.Windows.Forms.TextBox albumTitleTextBox;
         private System.Windows.Forms.Label tagRecommandations;
         private System.Windows.Forms.Label tagBestAlbums;
         private System.Windows.Forms.Label mostBorrowedAlbumsLabel;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox goToAcountView;
     }
 }
