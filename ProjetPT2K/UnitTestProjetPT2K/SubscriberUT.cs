59456b16c0835fd263d5e39281aa91d2007e9b25
Moins Alexis <alexis.moins@etu.u-bordeaux.fr>
2021-06-08 14:23:42 +0200

    Finished UT for US 1

--- ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
+++ ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
@@ -5,60 +5,87 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProjetPT2K
 {
+    /// <summary>
+    /// Unit tests for the class Subscriber.
+    /// </summary>
     [TestClass]
     public class SubscriberUT : UnitTest
     {
-        /**
-         * The subscriber used in the test.
-         */
+        /// <summary>
+        /// The subscriber used in the test.
+        /// </summary>
         private ABONNÉS _Subscriber;
 
-        /**
-         * Call the unit tests in an ordered manner.
-         */
+        /// <summary>
+        /// Call the unit tests in an ordered manner.
+        /// </summary>
         [TestMethod]
         public void OrderedUnitTests()
         {
             RestoreCleanState();
             CreateAccount();
             BorrowAlbum();
+            ListAlbums();
         }
 
-        /**
-         * Restore the database to a clean state for unit tests.
-         */
+        /// <summary>
+        /// Restore the database to a clean state for unit tests.
+        /// </summary>
         private void RestoreCleanState()
         {
             this.Database.RestoreCleanState();
             // Ensures the database is empty
-            Assert.IsFalse(this.Database.AccountExists("test"));
+            Assert.IsFalse(this.Database.AccountExists("jean"));
         }
 
-        /**
-         * Attempt to create a new account in the database.
-         */
+        /// <summary>
+        /// Attempt to create a new account in the database.
+        /// </summary>
         private void CreateAccount()
         {
-            this.Database.CreateAccount("Test", "Unitaire", 1, "test", "unitaire");
-            Account subscriber = this.Database.Login("test", "unitaire");
+            this.Database.CreateAccount("Jean", "Pierre", 1, "jean", "pierre");
+            Account subscriber = this.Database.Login("jean", "pierre");
             this._Subscriber = (ABONNÉS)subscriber;
-            
+
             Assert.IsNotNull(this._Subscriber);
             Assert.IsFalse(this._Subscriber.IsAdministrator);
 
-            Assert.AreEqual("Test", this._Subscriber.PRÉNOM_ABONNÉ);
-            Assert.AreEqual("Unitaire", this._Subscriber.NOM_ABONNÉ);
+            Assert.AreEqual("Jean", this._Subscriber.PRÉNOM_ABONNÉ);
+            Assert.AreEqual("Pierre", this._Subscriber.NOM_ABONNÉ);
 
-            Assert.AreEqual("test", this._Subscriber.LOGIN_ABONNÉ);
-            Assert.AreEqual("unitaire", this._Subscriber.PASSWORD_ABONNÉ);
+            Assert.AreEqual("jean", this._Subscriber.LOGIN_ABONNÉ);
+            Assert.AreEqual("pierre", this._Subscriber.PASSWORD_ABONNÉ);
 
             Assert.AreEqual(1, this._Subscriber.CODE_PAYS);
         }
 
+        /// <summary>
+        /// Attempt to borrow an album from the database.
+        /// </summary>
         private void BorrowAlbum()
         {
+            Assert.AreEqual(0, this._Subscriber.EMPRUNTER.Count);
             ALBUMS theAlbum = (from album in this.Connection.ALBUMS
-                            select album).FirstOrDefault();
+                               where album.CODE_ALBUM == 1
+                               select album).FirstOrDefault();
+
+            Assert.IsNotNull(theAlbum);
+            this._Subscriber.BorrowAlbum(theAlbum);
+
+            Assert.AreEqual(1, this._Subscriber.EMPRUNTER.Count);
+            EMPRUNTER loan = this._Subscriber.EMPRUNTER.First();
+
+            Assert.AreEqual(1, loan.CODE_ALBUM);
+            Assert.AreEqual(this._Subscriber.CODE_ABONNÉ, loan.CODE_ABONNÉ);
+        }
+
+        private void ListAlbums()
+        {
+            ALBUMS theAlbum = (from album in this.Connection.ALBUMS
+                               where album.CODE_ALBUM == 1
+                               select album).FirstOrDefault();
+
+            
         }
 
     }
