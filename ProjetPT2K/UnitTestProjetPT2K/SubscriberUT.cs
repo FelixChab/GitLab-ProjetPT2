6eb0be4903cc9c6300aa016a457afce2fdec552a
manboisson <manon.boisson.1@iut.u-bordeaux.fr>
2021-06-08 11:35:37 +0200

    SubscriberUT

---
+++ ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProjetPT2K
+{
+    [TestClass]
+    public class UnitTest1
+
+
+
+
+
+        [TestMethod]
+    public void CreateAccount()
+    {
+        
+    }
+
+
+}
+}
