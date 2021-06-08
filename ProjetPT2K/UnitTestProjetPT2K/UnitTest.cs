416abfe18521d0649e28ccd7a8770391cce0745b
Moins Alexis <alexis.moins@etu.u-bordeaux.fr>
2021-06-08 12:00:07 +0200

    Created UT based class

---
+++ ProjetPT2K/UnitTestProjetPT2K/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetPT2K;
+
+namespace UnitTestProjetPT2K
+{
+    public class UnitTest
+    {
+        /**
+         * The Database object the tests will use.
+         */
+        protected Database Database = Database.GetInstance();
+
+        /**
+         * The connection to the dabase.
+         */
+        protected MusiquePT2_KEntities Connection = Database.GetInstance().GetConnection();
+
+    }
+}
