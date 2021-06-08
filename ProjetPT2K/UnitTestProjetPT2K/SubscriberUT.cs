fec7e664c38d760fbbd6ae3d932c81e09cce4a6b
manboisson <manon.boisson.1@iut.u-bordeaux.fr>
2021-06-08 11:59:54 +0200

    1 st version of CreateAccount UT

--- ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
+++ ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using ProjetPT2K;
 
 namespace UnitTestProjetPT2K
 {
     [TestClass]
     public class UnitTest1
+    {
 
 
 
 
 
         [TestMethod]
-    public void CreateAccount()
-    {
-        
+        public void CreateAccount()
+        {
+            MusiquePT2_KEntities connection = Database.GetInstance().GetConnection();
+            Database.GetInstance().CreateAccount("prénom", "nom", 01, "testeur", "blabla");
+            connection.Database.ExecuteSqlCommand("SELECT NOM_ABONNÉ FROM ABONNÉS WHERE ABONNÉS.NOM_ABONNÉ = 'nom'");
+           
+        }
+
+
     }
-
-
-}
 }
