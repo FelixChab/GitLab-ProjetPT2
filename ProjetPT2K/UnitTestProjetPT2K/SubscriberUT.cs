08be8127850a3c28f340bd902f0b5d6ee85c429e
Moins Alexis <alexis.moins@etu.u-bordeaux.fr>
2021-06-08 12:00:07 +0200

    Created UT based class

--- ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
+++ ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
@@ -1,21 +1,37 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
+using ProjetPT2K;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProjetPT2K
 {
     [TestClass]
-    public class UnitTest1
+    public class SubscriberUT : UnitTest
+    {
+        /**
+         * The subscriber used in the test.
+         */
+        private ABONNÉS _Subscriber;
 
-
-
-
+        /**
+         * Restore the database to a clean state for Unit Testing.
+         */
+        [TestMethod]
+        public void RestoreCleanState()
+        {
+            this.Database.RestoreCleanState();
+        }
 
         [TestMethod]
-    public void CreateAccount()
-    {
-        
+        public void CreateAccount()
+        {
+
+        }
+
+        [TestMethod]
+        public void BorrowAlbum()
+        {
+
+        }
+
     }
-
-
-}
 }
