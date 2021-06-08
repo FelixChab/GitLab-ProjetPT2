aa2f672a9f164b1c78679082a3876e79ad7e4227
pbernini <paul.bernini@etu.u-bordeaux.fr>
2021-06-08 17:13:11 +0200

    Test

--- ProjetPT2K/ProjetPT2K/MainView.designer.cs
+++ ProjetPT2K/ProjetPT2K/MainView.designer.cs
@@ -29,7 +29,10 @@ namespace ProjetPT2K
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainView));
             this.recommandationsLabel = new System.Windows.Forms.Label();
+            this.mainMenuListBox = new System.Windows.Forms.ListBox();
+            this.albumTitleTextBox = new System.Windows.Forms.TextBox();
             this.tagRecommandations = new System.Windows.Forms.Label();
             this.tagBestAlbums = new System.Windows.Forms.Label();
             this.mostBorrowedAlbumsLabel = new System.Windows.Forms.Label();
@@ -51,6 +54,25 @@ namespace ProjetPT2K
             this.recommandationsLabel.Size = new System.Drawing.Size(185, 29);
             this.recommandationsLabel.TabIndex = 9;
             this.recommandationsLabel.Text = "Recommandations";
+            // 
+            // mainMenuListBox
+            // 
+            this.mainMenuListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(129)))), ((int)(((byte)(172)))));
+            this.mainMenuListBox.FormattingEnabled = true;
+            this.mainMenuListBox.Location = new System.Drawing.Point(12, 12);
+            this.mainMenuListBox.Name = "mainMenuListBox";
+            this.mainMenuListBox.Size = new System.Drawing.Size(179, 433);
+            this.mainMenuListBox.TabIndex = 10;
+            this.mainMenuListBox.SelectedIndexChanged += new System.EventHandler(this.mainMenuListBox_SelectedIndexChanged);
+            // 
+            // albumTitleTextBox
+            // 
+            this.albumTitleTextBox.Enabled = false;
+            this.albumTitleTextBox.Location = new System.Drawing.Point(231, 12);
+            this.albumTitleTextBox.Name = "albumTitleTextBox";
+            this.albumTitleTextBox.Size = new System.Drawing.Size(372, 20);
+            this.albumTitleTextBox.TabIndex = 11;
+            this.albumTitleTextBox.Visible = false;
             // 
             // tagRecommandations
             // 
@@ -155,6 +177,8 @@ namespace ProjetPT2K
             this.Controls.Add(this.mostBorrowedAlbumsLabel);
             this.Controls.Add(this.tagBestAlbums);
             this.Controls.Add(this.tagRecommandations);
+            this.Controls.Add(this.albumTitleTextBox);
+            this.Controls.Add(this.mainMenuListBox);
             this.Controls.Add(this.recommandationsLabel);
             this.Name = "MainView";
             this.Text = "MainView";
@@ -171,6 +195,8 @@ namespace ProjetPT2K
         #endregion
 
         private System.Windows.Forms.Label recommandationsLabel;
+        private System.Windows.Forms.ListBox mainMenuListBox;
+        private System.Windows.Forms.TextBox albumTitleTextBox;
         private System.Windows.Forms.Label tagRecommandations;
         private System.Windows.Forms.Label tagBestAlbums;
         private System.Windows.Forms.Label mostBorrowedAlbumsLabel;
