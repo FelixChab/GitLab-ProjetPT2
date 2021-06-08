7f70523c10a59bed4a828f9efba1d1b27bd2ac01
Moins Alexis <alexis.moins@etu.u-bordeaux.fr>
2021-06-08 13:55:53 +0200

    Adjustments in UT

--- ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
+++ ProjetPT2K/UnitTestProjetPT2K/SubscriberUT.cs
@@ -1,7 +1,8 @@
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using ProjetPT2K;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProjetPT2K
 {
@@ -14,27 +15,53 @@ namespace UnitTestProjetPT2K
         private ABONNÉS _Subscriber;
 
         /**
-         * Restore the database to a clean state for Unit Testing.
+         * Call the unit tests in an ordered manner.
          */
         [TestMethod]
-        public void RestoreCleanState()
+        public void OrderedUnitTests()
+        {
+            RestoreCleanState();
+            CreateAccount();
+            BorrowAlbum();
+        }
+
+        /**
+         * Restore the database to a clean state for unit tests.
+         */
+        private void RestoreCleanState()
         {
             this.Database.RestoreCleanState();
+            // Ensures the database is empty
+            Assert.IsFalse(this.Database.AccountExists("test"));
         }
 
-        public void CreateAccount()
+        /**
+         * Attempt to create a new account in the database.
+         */
+        private void CreateAccount()
         {
-            MusiquePT2_KEntities connection = Database.GetInstance().GetConnection();
-            Database.GetInstance().CreateAccount("prénom", "nom", 01, "testeur", "blabla");
-            connection.Database.ExecuteSqlCommand("SELECT NOM_ABONNÉ FROM ABONNÉS WHERE ABONNÉS.NOM_ABONNÉ = 'nom'");
+            this.Database.CreateAccount("Test", "Unitaire", 1, "test", "unitaire");
+            Account subscriber = this.Database.Login("test", "unitaire");
+            this._Subscriber = (ABONNÉS)subscriber;
+            
+            Assert.IsNotNull(this._Subscriber);
+            Assert.IsFalse(this._Subscriber.IsAdministrator);
 
+            Assert.AreEqual("Test", this._Subscriber.PRÉNOM_ABONNÉ);
+            Assert.AreEqual("Unitaire", this._Subscriber.NOM_ABONNÉ);
+
+            Assert.AreEqual("test", this._Subscriber.LOGIN_ABONNÉ);
+            Assert.AreEqual("unitaire", this._Subscriber.PASSWORD_ABONNÉ);
+
+            Assert.AreEqual(1, this._Subscriber.CODE_PAYS);
         }
 
-        [TestMethod]
-        public void BorrowAlbum()
+        private void BorrowAlbum()
         {
-
+            ALBUMS theAlbum = (from album in this.Connection.ALBUMS
+                            select album).FirstOrDefault();
         }
 
     }
+
 }
