e77f401e928916dd408dfb22fe8d22d5c5367f90
Moins Alexis <alexis.moins@etu.u-bordeaux.fr>
2021-06-10 14:57:30 +0200

    Transition

--- ProjetPT2K/ProjetPT2K/Database.cs
+++ ProjetPT2K/ProjetPT2K/Database.cs
@@ -62,51 +62,44 @@ namespace ProjetPT2K
         /// <summary>
         /// Return the account retrieved from the database corresponding to the given credentials.
         /// </summary>
-        /// <param name="login"> the login of the account </param>
-        /// <param name="password"> the password of the account </param>
+        /// <param name="theLogin"> the login of the account </param>
+        /// <param name="thePassword"> the password of the account </param>
         /// <returns></returns>
-        public Account Login(string login, string password)
+        public Account Login(string theLogin, string thePassword)
         {
-            Account account;
-            if (login == Administrator.LOGIN && password == Administrator.PASSWORD)
-                account = new Administrator();
-            else
-                account = FetchSubscriberAccount(login, password);
-            return account;
+            if (theLogin == Administrator.LOGIN && thePassword == Administrator.PASSWORD)
+                return new Administrator();
+            return FetchSubscriberAccount(theLogin, thePassword);
         }
 
         /// <summary>
         /// Retrieve the subscriber account corresponding to the given login and password.
         /// </summary>
-        /// <param name="login"> the login of the subscriber </param>
-        /// <param name="password"> the password of the subscriber </param>
+        /// <param name="theLogin"> the login of the subscriber </param>
+        /// <param name="thePassword"> the password of the subscriber </param>
         /// <returns> an Account object </returns>
-        private Account FetchSubscriberAccount(string login, string password)
+        private Account FetchSubscriberAccount(string theLogin, string thePassword)
         {
-            ABONNÉS theSubscriber = (from subscriber in this.Connection.ABONNÉS
-                                     where (subscriber.LOGIN_ABONNÉ == login)
-                                     && (subscriber.PASSWORD_ABONNÉ == password)
-                                     select subscriber).FirstOrDefault();
-
-            return theSubscriber;
+            List<ABONNÉS> theSubscribers = this.Connection.ABONNÉS.ToList();
+            return theSubscribers.Find(subsbcriber =>
+                subsbcriber.LOGIN_ABONNÉ.Trim() == theLogin &&
+                subsbcriber.PASSWORD_ABONNÉ.Trim() == thePassword);
         }
 
         /// <summary>
         /// Return true if an account with the given login already exists in the database
         /// </summary>
-        /// <param name="login"> the login we are looking for </param>
+        /// <param name="theLogin"> the login we are looking for </param>
         /// <returns> a boolean </returns>
-        public bool AccountExists(string login)
+        public bool AccountExists(string theLogin)
         {
-            ABONNÉS account = (from subscriber in this.Connection.ABONNÉS
-                               where (subscriber.LOGIN_ABONNÉ == login)
-                               select subscriber).FirstOrDefault();
-
-            return account != null;
+            List<ABONNÉS> theSubscribers = this.Connection.ABONNÉS.ToList();
+            return theSubscribers.Find(subsbcriber =>
+                subsbcriber.LOGIN_ABONNÉ.Trim() == theLogin) != null;
         }
 
         /// <summary>
-        /// Verify the possibility to create a new subscriber account and creates it if possible.
+        /// Verify thepossibility to create a new subscriber account in the database. Creates it if possible.
         /// </summary>
         /// <param name="firstname"> the firstname of the user </param>
         /// <param name="lastname"> the lastname of the user </param>
@@ -115,17 +108,42 @@ namespace ProjetPT2K
         /// <param name="password"> the password of the user </param>
         public void AttemptAccountCreation(string firstname, string lastname, int countryCode, string login, string password)
         {
-            if (!AccountExists(login) && CredentialsAreValid(login, password))
-                CreateAccount(firstname, lastname, countryCode, login, password);
-            else
+            try
+            {
+                CredentialsAreValid(firstname, lastname, login, password);
+            } 
+            catch
+            {
+                throw;
+            }
+            if (AccountExists(login) || !CredentialsAreValid(firstname, lastname, login, password))
                 throw new Exception("Nom d'utilisateur indisponible");
+            CreateAccount(firstname, lastname, countryCode, login, password);
+        }
+
+        /// <summary>
+        /// Return true if the login and the password are valid.
+        /// </summary>
+        /// <param name="theCredentials"> the list of credentials (login & password) </param>
+        /// <returns></returns>
+        private bool CredentialsAreValid(params string[] theCredentials)
+        {
+            Regex theRegex = new Regex("^[A-Za-z0-9]*$");
+            foreach (string theCredential in theCredentials)
+            {
+                if (theCredential.Length == 0)
+                    throw new Exception("Les champs ne doivent pas être vides");
+                if (theCredential.Length > 32)
+                    throw new Exception("Les champs ne doivent pas dépasser 32 caractères");
+                if (!theRegex.IsMatch(theCredential))
+                    throw new Exception("Les champs ne peuvent contenir que des lettres et des chiffres");
+            }
+            return true;
         }
 
         /// <summary>
         /// Create a new subscriber account in the database
         /// </summary>
-        /// <param name="firstname"> the firstname of the user </param>
-        /// <param name="lastname"> the lastname of the user </param>
         /// <param name="countryCode"> the code of the user's country</param>
         /// <param name="login"> the login of the user </param>
         /// <param name="password"> the password of the user </param>
@@ -146,23 +164,6 @@ namespace ProjetPT2K
             this.Connection.SaveChanges();
         }
 
-        /// <summary>
-        /// Return true if the login and the password are valid.
-        /// </summary>
-        /// <param name="theCredentials"> the list of credentials (login & password) </param>
-        /// <returns></returns>
-        private bool CredentialsAreValid(params string[] theCredentials)
-        {
-            Regex theRegex = new Regex("^[A-Za-z0-9]*$");
-            foreach (string theCredential in theCredentials)
-            {
-                if (theCredential.Length == 0)
-                    throw new Exception("Vous devez renseigner un mot de passe et un nom d'utilisateur");
-                if (!theRegex.IsMatch(theCredential))
-                    throw new Exception("Le mot de passe et le nom d'utilisateur ne peux contenir que des lettres et des chiffres ");
-            }
-            return true;
-        }
 
         /// <summary>
         /// Return all the allbums of the database.
@@ -180,7 +181,7 @@ namespace ProjetPT2K
         /// <returns> a list of Albums objects </returns>
         public List<ALBUMS> GetAlbumsContaining(string thePattern)
         {
-            List<ALBUMS> theAlbums = this.Connection.ALBUMS.ToList();
+            List<ALBUMS> theAlbums = this.GetAllAlbums();
             return theAlbums.FindAll(album => album.TITRE_ALBUM.Contains(thePattern));
         }
 
@@ -190,27 +191,25 @@ namespace ProjetPT2K
         /// <returns> a list of Abonné objects </returns>
         public List<ABONNÉS> GetInactiveSubscribers()
         {
-            List<ABONNÉS> inactiveSubscribers = new List<ABONNÉS>();
+            List<ABONNÉS> theInactiveSubscribers = new List<ABONNÉS>();
             List<ABONNÉS> theSubscribers = this.Connection.ABONNÉS.ToList();
             foreach (ABONNÉS theSubscriber in theSubscribers)
             {
                 if (!theSubscriber.IsActive())
-                    inactiveSubscribers.Add(theSubscriber);
+                    theInactiveSubscribers.Add(theSubscriber);
             }
-            return inactiveSubscribers;
+            return theInactiveSubscribers;
         }
 
         /// <summary>
         /// Return the album corresponding to the given ID in the database.
         /// </summary>
-        /// <param name="ID"> the ID of the album </param>
+        /// <param name="theID"> the ID of the album </param>
         /// <returns> an Album object </returns>
-        public ALBUMS GetAlbumWithID(int ID)
+        public ALBUMS GetAlbumWithID(int theID)
         {
-            ALBUMS theAlbum = (from album in this.Connection.ALBUMS
-                               where album.CODE_ALBUM == ID
-                               select album).FirstOrDefault();
-            return theAlbum;
+            List<ALBUMS> theAlbums = this.GetAllAlbums();
+            return theAlbums.Find(theAlbum => theAlbum.CODE_ALBUM == theID);
         }
 
         /// <summary>
@@ -220,8 +219,9 @@ namespace ProjetPT2K
         /// <returns> a dictionnary of Album objects and int </returns>
         public Dictionary<ALBUMS, int> GetMostBorrowedAlbumsOfGenre(GENRES theGenre)
         {
+            List<EMPRUNTER> theLoans = this.Connection.EMPRUNTER.ToList();
             Dictionary<ALBUMS, int> topAlbums = new Dictionary<ALBUMS, int>();
-            foreach (EMPRUNTER theLoan in this.Connection.EMPRUNTER)
+            foreach (EMPRUNTER theLoan in theLoans)
             {
                 if (topAlbums.ContainsKey(theLoan.ALBUMS))
                     topAlbums[theLoan.ALBUMS]++;
@@ -238,8 +238,9 @@ namespace ProjetPT2K
         /// <returns> a dictionnary of Album objects and int </returns>
         public Dictionary<ALBUMS, int> GetMostBorrowedAlbums()
         {
+            List<EMPRUNTER> theLoans = this.Connection.EMPRUNTER.ToList();
             Dictionary<ALBUMS, int> topAlbums = new Dictionary<ALBUMS, int>();
-            foreach (EMPRUNTER theLoan in this.Connection.EMPRUNTER)
+            foreach (EMPRUNTER theLoan in theLoans)
             {
                 if (topAlbums.ContainsKey(theLoan.ALBUMS))
                     topAlbums[theLoan.ALBUMS]++;
